823a4361b4988768db1333e604bce829e825bc44
uqkip <iris3price9@gmail.com>
2020-04-19 19:19:06 +0200

    Progress on ID

--- src/main/scala/Datapath.scala
+++ src/main/scala/Datapath.scala
@@ -1,8 +1,10 @@
 import chisel3._
+import chisel3.util._
 
 class Datapath {
   val iW = 32;  // 32 bit instructions
   val aW = 8;   // 8 bit addresses in instruction memory
+  val rW = 4;   // 4 bit addresses for registers
   val vW = 32;  // 32 bit immediates
 
   // read-only instruction memory
@@ -26,7 +28,7 @@ class Datapath {
     val pc = RegInit(0.U(aW.W)) // program counter starts at 0
     pc := pc + 4.U(aW.W)        // increase by 4 each clock cycle (since instructions are 4 bytes)
 
-    val iMem = Module(new InstructionMem) // create instance of instruction memory
+    val iMem = Module(new InstructionMem) // create instance of instruction memory - should already exist?
     iMem.io.rdAddr := pc                  // set memory read address to pc
     io.instruction := iMem.io.rdData      // set instruction to be output of memory read
   }
@@ -34,13 +36,35 @@ class Datapath {
   class instruction_decode extends Module {
     val io = IO(new Bundle {
       val instruction = Input(UInt(iW.W))
-      val a           = Output(UInt(vW.W))
-      val b           = Output(UInt(vW.W))
-      val imm         = Output(UInt(vW.W))
+      val dest        = Output(UInt(rW.W))  // destination register address for instruction
+      val a           = Output(UInt(vW.W))  // value retrieved from reg
+      val b           = Output(UInt(vW.W))  // value retrieved from reg
+      val imm         = Output(UInt(vW.W))  // padded value from instruction
+      val opcode      = Output(UInt(5.W))
+      val select      = Output(UInt(1.W))   // for the MUX between imm and b
     })
 
-    // parse opcode
-    // switch statement on opcode
+    // parse components of instruction
+    io.opcode := io.instruction(0,4)    // opcode is first 5 bits of instruction
+    switch(io.opcode(0)){               // first digit of opcode determines instruction type
+
+      is(0.U(1.W)){     // ALU instruction
+
+        val scode = io.instruction(5,7)        // select code
+        val dest = io.instruction(8,11)        // destination register
+        val a_addr = io.instruction(12,15)     // source register a
+
+        when(scode === 0.U(3.W)){           // a + b reg used
+          val b_addr = io.instruction(16,19)
+        } .elsewhen(scode === 1.U(3.W)){    // a reg + imm used
+          io.imm := Cat(0.U(16.W), io.instruction(16,31)) // pad immediate to 32 bits
+        }
+      }
+
+      is(1.U(1.W)){     // memory instruction
+
+      }
+    }
         // parse & extend immediate if necessary
         // read values of a and b from register memory
   }
