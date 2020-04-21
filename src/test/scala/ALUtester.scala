12829d23156f859108828a9cbdacae6ffff989f6
Simone Bystadhagen <simonby@stud.ntnu.no>
2020-04-21 16:06:47 +0200

    tester is now working and used on all alu operations

--- src/test/scala/ALUtester.scala
+++ src/test/scala/ALUtester.scala
@@ -4,19 +4,27 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ALUtest(alu: ALU) extends PeekPokeTester(alu){
   val a = 1
-  val b = 1
+  val b = 2
   val sel = 0
-  val opcode = 1
-  val res = 2
-
+  val opcodes = Vector(0,1,2,3,4,5,6,7,8)
+  val results = Vector(1,2,0,3,-1,-2,0, 3, 3)
+  var opcode = 0
+  var res = 0
   poke(alu.io.a, a)
   poke(alu.io.b, b)
-  poke(alu.io.select, sel)
-  poke(alu.io.opcode, opcode)
+  for ( i <- 0 until 9){
 
-  step(1)
+    opcode = opcodes(i)
+    res = results(i)
 
-  expect(alu.io.out, res)
+    poke(alu.io.select, sel)
+    poke(alu.io.opcode, opcode)
+
+    step(1)
+
+    expect(alu.io.out, res)
+
+  }
 
 }
 
